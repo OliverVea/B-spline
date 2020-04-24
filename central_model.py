3d74f6362e4ba85cfe0c654913e7f09123f4c2aa
Olvea16 <olvea16@student.sdu.dk>
2020-04-24 10:35:11 +0200

    Added function docstrings.

--- central_model.py
+++ central_model.py
@@ -21,9 +21,11 @@ class CentralModel:
         Keyword arguments: \n
         image_dimensions: tuple containing (width, height) of image in pixels. \n
         grid_dimensions: tuple containing (width, height) of grid in pixels. \n
-        grid: numpy 3d array containing unit vectors describing camera intrinsics.\n
+        control_points: numpy 3d array containing unit vectors describing camera intrinsics.\n
         order: the order of the interpolation.\n
-        min_basis_value: the minimum basis value 
+        knot_method: the method used to generate the knot vector.\n
+        min_basis_value: the minimum basis value. Used for optimization.\n
+        end_divergence: a small term added to the tails of the knot vector when 'open_uniform' is used. Allows for sampling at the endpoints of the spline.
         """
         assert knot_method in ['open_uniform', 'uniform'], 'knot method should be one of the implemented methods.'
         assert isinstance(image_dimensions, tuple) and len(image_dimensions) == 2, 'image_dimensions must be a 2-dimensional tuple.'
@@ -100,6 +102,13 @@ class CentralModel:
         return B
 
     def normalize(self, coord, grid_size, image_size):
+        """Returns normalized coordinates. \n
+
+        Keyword arguments: \n
+        coord: Pixel coordinate in one axis.\n
+        grid_size: the grid size in pixels in the same axis.\n
+        image_size: the image size in pixels in the same axis.
+        """
         t = coord + (grid_size - image_size) / 2
         t /= grid_size - 1
 
@@ -131,6 +140,8 @@ class CentralModel:
         return res
 
     def sample_grid(self):
+        """Used to sample the b-spline surface in the input coordinates corresponding to the control points. \n
+        """
         xs = np.floor((self.grid_width  - 1) / (self.n - 1) * np.arange(0, self.n))
         ys = np.floor((self.grid_height - 1) / (self.m - 1) * np.arange(0, self.m))
 
@@ -143,12 +154,15 @@ class CentralModel:
 
         return samples
 
-    def task(self, i, pts, output):
-        #if i == 0: pts = tqdm(pts)
-        r = output.put(np.array([np.hstack([pt[0], self.sample(pt[1], pt[2])]) for pt in pts]))
-        #print('Thread {} done.'.format(i))
+    def _task(self, i, pts, output): output.put(np.array([np.hstack([pt[0], self.sample(pt[1], pt[2])]) for pt in pts]))
 
-    def sample_many(self, pts, threads):
+    def sample_many(self, pts, threads=1):
+        """Used to sample the b-spline surface at multiple points. This method is multi-threaded and will start additional processes even when thread is set to 1. \n
+
+        Keyword arguments: \n
+        pts: (n, 2) array of pixel coordinates. \n
+        threads: Amount of processes to spawn. 
+        """
         results = multiprocessing.Queue()
 
         _pts = np.ndarray((len(pts), 3))
@@ -157,7 +171,7 @@ class CentralModel:
 
         split = np.split(_pts, threads)
 
-        processes = [multiprocessing.Process(target=self.task, args=(i, split[threads - i - 1], results)) for i in range(threads)]
+        processes = [multiprocessing.Process(target=self._task, args=(i, split[threads - i - 1], results)) for i in range(threads)]
 
         for process in processes: 
             process.start()
@@ -171,6 +185,12 @@ class CentralModel:
         return results[:,:-1]
 
     def active_control_points(self, u, v):
+        """Returns the indeces of the control points that are used to calculate the point with the given pixel coordinates. \n
+
+        Keyword arguments: \n
+        u: Horizontal pixel coordinate. \n
+        v: Vertical pixel coordinate. 
+        """
         is_even = lambda x: x % 2 == 0
         
         px = len(self.th) * self.normalize(u, self.grid_width, self.image_width)  
@@ -186,7 +206,20 @@ class CentralModel:
             
         return np.transpose(np.meshgrid(x, y))
     
-def fit_central_model(target_values, img_shape, grid_shape, order = 3, knot_method = 'open_uniform', end_divergence = 0, min_basis_value = 1e-3, verbose = 0, initial_values = None):
+def fit_central_model(target_values, image_dimensions, grid_dimensions, order = 3, knot_method = 'open_uniform', end_divergence = 0, min_basis_value = 1e-3, verbose = 0, initial_values = None):
+    """Used to fit the control points so the spline interpolates between the values in 'target_values'. Returns the fitted CentralModel and the least squares fitting results. \n
+
+    Keyword arguments: \n
+    target_values: The values the solver will try to fit the b-spline surface to.\n
+    image_dimensions: see CentralModel's initialization.\n
+    grid_dimensions: see CentralModel's initialization.\n
+    order: see CentralModel's initialization.\n
+    knot_method: see CentralModel's initialization.\n
+    end_divergence: see CentralModel's initialization.\n
+    min_basis_value: see CentralModel's initialization.\n
+    verbose: Changes how much the function prints while running.\n
+    initial_values: Optional numpy array with the shape of 'target_values'. Provides the initial control points to the least squares solver.\n
+    """
     if initial_values == None: 
         initial_values = target_values
 
@@ -218,8 +251,8 @@ def fit_central_model(target_values, img_shape, grid_shape, order = 3, knot_meth
     ctrl = result['x'].reshape(shape)
 
     cm = CentralModel(
-        image_dimensions=img_shape, 
-        grid_dimensions=grid_shape, 
+        image_dimensions=image_dimensions, 
+        grid_dimensions=grid_dimensions, 
         control_points=ctrl, 
         order=order,
         knot_method=knot_method,
@@ -228,9 +261,10 @@ def fit_central_model(target_values, img_shape, grid_shape, order = 3, knot_meth
 
     return cm, result
 
-import time
 
 if __name__ == '__main__':
+    import time
+
     x = np.arange(2000)
     y = np.arange(2000)
     pts = np.transpose(np.meshgrid(x,y)).reshape(-1, 2)
@@ -239,19 +273,14 @@ if __name__ == '__main__':
 
     start = time.time()
     cm = CentralModel((2000,2000), (2000,2000), np.random.normal(0, 1, (6,6,3)), 4)
-    _ = cm.sample_many(pts, 1)
-    end = time.time()
-
-    print('Non-threaded version used {%01d} s. ({%01d} iter/s)'.format(end - start, len(pts) / (end - start)))
-
-    start = time.time()
-    cm = CentralModel((2000,2000), (2000,2000), np.random.normal(0, 1, (6,6,3)), 4)
     _ = cm.sample_many(pts, 16)
     end = time.time()
 
-    print('Threaded version used {%01d} s. ({%01d} iter/s)'.format(end - start, len(pts) / (end - start)))
+    print('Threaded version used {} s. ({} iter/s)'.format(end - start, len(pts) / (end - start)))
 
-    print(pts.shape)
+    start = time.time()
+    cm = CentralModel((2000,2000), (2000,2000), np.random.normal(0, 1, (6,6,3)), 4)
+    _ = cm.sample_many(pts, 1)
+    end = time.time()
 
-
-    pass
+    print('Non-threaded version used {} s. ({} iter/s)'.format(end - start, len(pts) / (end - start)))
